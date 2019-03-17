cae85adef26a66f158ead02fd949cff5ed04b2ea
Nicolas Sieiro <sieiro.nicolas@gmail.com>
2019-03-16 22:55:23 -0300

    add firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+  // Initialize Firebase
+var config = {
+    apiKey: 'AIzaSyDec1VTdqQlY_wVvtdMv3DcXJ0xKc6gcuM',
+    authDomain: "instagram-clon-a6bda.firebaseapp.com",
+    databaseURL: "https://instagram-clon-a6bda.firebaseio.com",
+    projectId: "instagram-clon-a6bda",
+    storageBucket: "instagram-clon-a6bda.appspot.com",
+    messagingSenderId: "823363455837"
+  };
+  // eslint-disable-next-line
+  firebase.initializeApp(config)
